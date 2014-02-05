722594038a47937d909fbd2a451b497532667a80
Thomas Lockney <thomas@lockney.net>
2014-02-05 11:32:43 -0800

    improved metrics tracking and thread-safety just a bit

--- src/main/scala/net/lockney/MetricsActor.scala
+++ src/main/scala/net/lockney/MetricsActor.scala
@@ -4,10 +4,8 @@ import akka.actor.{ActorLogging, Actor}
 import java.net.URL
 import spray.json.DefaultJsonProtocol
 import com.codahale.metrics.{JmxReporter, MetricRegistry}
-
-case class EmojiStat(name: String, count: Long = 0)
-case class HashtagStat(tag: String, count: Long = 0)
-case class UrlStat(url: URL, count: Long = 0)
+import scala.collection.mutable.TreeSet
+import java.util.concurrent.atomic.AtomicLong
 
 case class StatsSummary(
                          totalTweets:       Long,
@@ -19,37 +17,46 @@ case class StatsSummary(
                          urlsSeen:          Long,
                          urlsToTweetRatio:  Double,
                          pictureUrlRatio:   Double,
-                         topEmojis:         Seq[(String, Long)],
-                         topHashtags:       Seq[(String, Long)],
-                         topDomains:        Seq[(String, Long)]
+                         topEmojis:         Set[(String, Long)],
+                         topHashtags:       Set[(String, Long)],
+                         topDomains:        Set[(String, Long)]
                          )
 
 object StatsSummaryProtocol extends DefaultJsonProtocol {
-  implicit val emojiCountFormat   = jsonFormat2(EmojiStat)
-  implicit val hashtagCountFormat = jsonFormat2(HashtagStat)
   implicit val statsSummaryFormat = jsonFormat12(StatsSummary)
 }
 
 object MetricsActor {
 
+  protected[this] class Stat(val name: String, var count: AtomicLong = new AtomicLong(1)) {
+    def currentCount = count.get
+  }
+
   val name = "metrics"
 
   case object TweetSeen
-  case class EmojiSeen(name: String)
+  case class EmojiSeen(emoji: String)
   case class HashtagSeen(tag: String)
   case class UrlSeen(url: URL)
 
-  private var emojiStats: Map[String, EmojiStat]     = Map.empty
-  private var hashtagStats: Map[String, HashtagStat] = Map.empty
-  private var urlStats: Map[URL, UrlStat]            = Map.empty
+  implicit val statOrdering = new Ordering[Stat] {
+    def compare(e1: Stat, e2: Stat) = {
+      val count = (e1.count.get - e2.count.get).toInt
+      if (count != 0) count else e1.name.compareTo(e2.name)
+    }
+  }
+
+  private val emojiStats   = TreeSet[Stat]()
+  private val hashtagStats = TreeSet[Stat]()
+  private val urlStats     = TreeSet[Stat]()
 
   private def counterFor(name: String) = registry.counter(name)
   private def meterFor(name: String)   = registry.meter(MetricRegistry.name(name))
 
-  protected[this] lazy val tweetMeter  = meterFor("net.lockney.tweets.meter")
-  protected[this] lazy val emojisSeen  = counterFor("net.lockney.emojisSeen")
-  protected[this] lazy val urlsSeen    = counterFor("net.lockney.urlsSeen")
-  protected[this] lazy val picsSeen    = counterFor("net.lockney.picsSeen")
+  private lazy val tweetMeter  = meterFor("net.lockney.tweets.meter")
+  private lazy val emojisSeen  = counterFor("net.lockney.emojisSeen")
+  private lazy val urlsSeen    = counterFor("net.lockney.urlsSeen")
+  private lazy val picsSeen    = counterFor("net.lockney.picsSeen")
 
   private lazy val registry = {
     val reg = new MetricRegistry()
@@ -59,33 +66,35 @@ object MetricsActor {
 
   def countTweet = tweetMeter.mark
 
-  /**
-   * Note: this is *NOT* thread-safe. Either call it from a single thread or go home!
-   */
-  def countHashtag(s: String) {
-    val stats = hashtagStats.getOrElse(s, HashtagStat(s))
-    val newStats = stats.copy(count = stats.count + 1)
-    hashtagStats = hashtagStats.updated(s, newStats)
+  private def updateStat(item: String, stats: TreeSet[Stat]) {
+    stats.find(c => c.name == item) match {
+      case Some(stat) => stat.count.incrementAndGet(); stats.update(stat, true)
+      case None => stats += new Stat(item)
+    }
   }
 
   /**
    * Note: this is *NOT* thread-safe. Either call it from a single thread or go home!
    */
-  def countEmoji(s: String) {
+  protected def countHashtag(item: String) = updateStat(item, hashtagStats)
+
+  /**
+   * Note: this is *NOT* thread-safe. Either call it from a single thread or go home!
+   */
+  protected def countEmoji(item: String) {
     emojisSeen.inc
-    val stats = emojiStats.getOrElse(s, EmojiStat(s))
-    val newStats = stats.copy(count = stats.count  + 1)
-    emojiStats = emojiStats.updated(s, newStats)
+    updateStat(item, emojiStats)
   }
 
   private val picDomains = Set("pic.twitter.com", "instagram.com")
 
-  def countUrl(u: URL) {
+  /**
+   * Note: this is *NOT* thread-safe. Either call it from a single thread or go home!
+   */
+  protected def countUrl(u: URL) {
     urlsSeen.inc
-    val stats = urlStats.getOrElse(u, UrlStat(u))
-    val newStats = stats.copy(count = stats.count + 1)
-    urlStats = urlStats.updated(u, newStats)
-    picDomains.foreach(pd => if (u.getHost.endsWith(pd)) picsSeen.inc)
+    updateStat(u.getHost, urlStats)
+    picDomains.foreach(pd => if (u.getHost.toLowerCase.endsWith(pd)) picsSeen.inc)
   }
 
   val statSorter = (c1: Product2[_, Long], c2: Product2[_, Long]) => c1._2 > c2._2
@@ -100,17 +109,9 @@ object MetricsActor {
     val urlRatio   = if (urlCount < 1) 0 else (urlCount / tweetCount.toDouble)
     val picRatio   = if (picCount < 1) 0 else (picCount / tweetCount.toDouble)
 
-    val topEmojis = emojiStats.map {
-      case (name, stats) => (name, stats.count)
-    }.toSeq.sortWith(statSorter).take(5)
-
-    val topHashtags = hashtagStats.map {
-      case (name, stats) => (name, stats.count)
-    }.toSeq.sortWith(statSorter).take(5)
-
-    val topDomains = urlStats.map {
-      case (url, stats) => (url.getHost, stats.count)
-    }.toSeq.sortWith(statSorter).take(5)
+    val topEmojis = emojiStats.take(5).map(s => (s.name, s.currentCount)).toSet
+    val topHashtags = hashtagStats.take(5).map( s => (s.name, s.currentCount)).toSet
+    val topDomains = urlStats.take(5).map(s => (s.name, s.currentCount)).toSet
 
     StatsSummary(
       tweetCount,
@@ -139,7 +140,6 @@ class MetricsActor extends Actor with ActorLogging {
 
     case EmojiSeen(name) =>
       countEmoji(name)
-
 
     case HashtagSeen(tag) =>
       countHashtag(tag)
